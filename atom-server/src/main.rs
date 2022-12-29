f97699cadb8fba08caf140a8cfe2a45fe166a853
Mike Moran <mike@houseofmoran.com>
2022-12-29 18:22:41 +0100

    very very basic example

--- atom-server/src/main.rs
+++ atom-server/src/main.rs
@@ -1,3 +1,11 @@
+extern crate atom_syndication;
+
+use atom_syndication::Feed;
+use atom_syndication::Entry;
+
 fn main() {
-    println!("Hello, world!");
+    let mut feed = Feed::default();
+    let entries = vec![Entry::default()];
+    feed.set_entries(entries);
+    println!("feed: {:?}", feed);
 }
