5de77eca59e8b57e89c67e7f8a91e94171bd8875
Mike Moran <mike@houseofmoran.com>
2022-12-29 22:34:52 +0100

    get all subjects

--- pragmatic/src/main.rs
+++ pragmatic/src/main.rs
@@ -1,9 +1,9 @@
 use std::env;
-use atom_syndication::Feed;
+use atom_syndication::{Feed, Entry};
 use std::io::Write;
 use std::fs::OpenOptions;
 use std::net::TcpStream;
-
+use imap::Session;
 
 fn main() -> Result<(), ()> {
     let args: Vec<String> = env::args().collect();
@@ -18,6 +18,13 @@ fn main() -> Result<(), ()> {
     let mut feed = Feed::default();
     feed.set_title(format!("Feed for '{}'", email));
 
+    let mut imap_session = open_session(&domain,port, &username, &password).unwrap();
+
+    let entries = fetch_entries(&mut imap_session, &email).unwrap();
+    feed.set_entries(entries);
+
+    imap_session.logout().unwrap();
+
     let mut output_file = OpenOptions::new()
         .write(true)
         .create(true)
@@ -25,16 +32,29 @@ fn main() -> Result<(), ()> {
         .open(output_file_path).unwrap();
     output_file.write_all(feed.to_string().as_bytes()).unwrap();
 
-    let mut imap_session = open_session(&domain,port, &username, &password).unwrap();
-
-    
-
-    imap_session.logout().unwrap();
-
     Ok(())
 }
 
-fn open_session(domain: &str, port: u16, username: &str, password: &str) -> imap::error::Result<imap::Session<TcpStream>> {
+fn fetch_entries(imap_session: &mut Session<TcpStream>, email: &str) -> imap::error::Result<Vec<Entry>> {
+    let sequences = imap_session.search(format!("FROM {}", email)).unwrap();
+    println!("sequences: {:?}", sequences);
+
+    let sequence_set = sequences.into_iter().map(|s| s.to_string()).collect::<Vec<String>>().join(",");
+    let messages = imap_session.fetch(
+        sequence_set, 
+        "(FLAGS INTERNALDATE RFC822.SIZE ENVELOPE BODYSTRUCTURE BODY.PEEK[TEXT])").unwrap();
+    for message in &messages {
+        let envelope = message.envelope().unwrap();
+        let subject = std::str::from_utf8(envelope.subject.unwrap())
+            .expect("was not valid utf-8")
+            .to_string();
+        println!("subject: '{}'", subject);
+    }
+   
+    Ok(vec![Entry::default()])
+}
+
+fn open_session(domain: &str, port: u16, username: &str, password: &str) -> imap::error::Result<Session<TcpStream>> {
     let stream = TcpStream::connect((domain, port)).unwrap();
     let client = imap::Client::new(stream);
 
