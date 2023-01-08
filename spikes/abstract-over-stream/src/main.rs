b945c206c025ffbdcc384af51890767b2a50f681
Mike Moran <mike@houseofmoran.com>
2023-01-08 17:52:00 +0000

    work the same as `gmail-spike`

--- spikes/abstract-over-stream/src/main.rs
+++ spikes/abstract-over-stream/src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+
+mod imap_session;
+mod config;
+
 fn main() {
-    println!("Hello, world!");
+    let config = config::from_path("config.toml");
+    let password = env::var("IMAP_PASSWORD").unwrap();
+    let args: Vec<String> = env::args().collect();
+    let sequence_set = &args[1];
+
+    let mut imap_session = 
+        imap_session::open_session(&config.imap.domain, config.imap.port, &config.imap.username, &password).unwrap();
+
+    let messages = imap_session.fetch(sequence_set, "RFC822").unwrap();
+    if let Some(message) = messages.iter().next() {
+        let body = message.body().expect("message did not have a body!");
+        let body = std::str::from_utf8(body)
+            .expect("message was not valid utf-8")
+            .to_string();
+        println!("message:");
+        println!("{}", body);
+    }
+
+    imap_session::close_session(imap_session);
 }
