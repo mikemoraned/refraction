7f6554e166966fe49e07a1f5838c3bb7fd08381e
Mike Moran <mike@houseofmoran.com>
2023-01-22 23:06:04 +0000

    unauth version

--- spikes/abstract-over-stream/src/imap_session.rs
+++ spikes/abstract-over-stream/src/imap_session.rs
@@ -1,25 +1,14 @@
-use std::{net::TcpStream, io::{Read, Write}};
-use native_tls::TlsStream;
+use std::net::TcpStream;
 use imap::Session;
 
-// pub type Stream = TlsStream<TcpStream>;
-
-pub fn open_session<S>(domain: &str, port: u16, username: &str, password: &str) 
-    -> imap::error::Result<Session<S>>
-where S: Read + Write {
-    let tls = native_tls::TlsConnector::builder().build().unwrap();
-
-    // let stream = TcpStream::connect((domain, port)).unwrap();
-    // let client = imap::Client::new(stream);
-
-    // we pass in the domain twice to check that the server's TLS
-    // certificate is valid for the domain we're connecting to.
-    let client = 
-        imap::connect((domain, port), domain, &tls).unwrap();
+pub fn open_session(domain: &str, port: u16, username: &str, password: &str) 
+    -> imap::error::Result<Session<TcpStream>> {
+    let stream = TcpStream::connect((domain, port)).unwrap();
+    let client = imap::Client::new(stream);
 
     // the client we have here is unauthenticated.
     // to do anything useful with the e-mails, we need to log in
-    let mut imap_session : Session<S> = client
+    let mut imap_session = client
         .login(username, password)
         .map_err(|e| e.0)?;
 
@@ -30,7 +19,6 @@ where S: Read + Write {
     Ok(imap_session)
 }
 
-pub fn close_session<S>(mut imap_session: Session<S>)
-where S: Read + Write {
+pub fn close_session(mut imap_session: Session<TcpStream>) {
     imap_session.logout().unwrap();
 }
