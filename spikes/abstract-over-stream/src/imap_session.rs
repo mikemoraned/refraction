a7179ad5318a4bb0f9ccbdc6b43a6c6f470a65d8
Mike Moran <mike@houseofmoran.com>
2023-01-23 22:47:39 +0000

    use `ReadWrite` trait for `open_session`

--- spikes/abstract-over-stream/src/imap_session.rs
+++ spikes/abstract-over-stream/src/imap_session.rs
@@ -2,9 +2,12 @@ use std::{net::TcpStream, io::{Read, Write}};
 use imap::Session;
 use native_tls::TlsStream;
 
+pub trait ReadWrite: Read + Write {}
+impl<T: Read + Write> ReadWrite for T {}
+
 pub fn open_session(domain: &str, port: u16, username: &str, password: &str) 
-    -> imap::error::Result<Session<TcpStream>> {
-    let stream = TcpStream::connect((domain, port)).unwrap();
+    -> imap::error::Result<Session<Box<dyn ReadWrite>>> {
+    let stream : Box<dyn ReadWrite> = Box::new(TcpStream::connect((domain, port)).unwrap());
     let client = imap::Client::new(stream);
 
     let mut session = client
@@ -20,24 +23,18 @@ pub fn open_tls_session(domain: &str, port: u16, username: &str, password: &str)
     -> imap::error::Result<Session<TlsStream<TcpStream>>> {
     let tls = native_tls::TlsConnector::builder().build().unwrap();
 
-    // we pass in the domain twice to check that the server's TLS
-    // certificate is valid for the domain we're connecting to.
     let client = 
         imap::connect((domain, port), domain, &tls).unwrap();
 
-    // the client we have here is unauthenticated.
-    // to do anything useful with the e-mails, we need to log in
     let mut imap_session = client
         .login(username, password)
         .map_err(|e| e.0)?;
 
-    // we want to fetch the first email in the INBOX mailbox
-    // imap_session.select("INBOX")?;
     imap_session.examine("INBOX")?;
 
     Ok(imap_session)
 }
 
-pub fn close<T: Read + Write>(mut session: Session<T>) {
+pub fn close<T: ReadWrite>(mut session: Session<T>) {
     session.logout().unwrap();
 }
