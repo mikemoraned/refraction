cd6d25c3d847c7d3f73a800c928a1b975ee20f2b
Mike Moran <mike@houseofmoran.com>
2023-01-23 00:07:00 +0000

    extract `ImapSession`

--- spikes/abstract-over-stream/src/imap_session.rs
+++ spikes/abstract-over-stream/src/imap_session.rs
@@ -1,26 +1,36 @@
-use std::net::TcpStream;
-use imap::Session;
+use std::{net::TcpStream, io::{Read, Write}};
+use imap::{Session, types::{Fetch, ZeroCopy}, Error};
 
-pub type ImapSession = Session<TcpStream>;
-
-pub fn open_session(domain: &str, port: u16, username: &str, password: &str) 
-    -> imap::error::Result<ImapSession> {
-    let stream = TcpStream::connect((domain, port)).unwrap();
-    let client = imap::Client::new(stream);
-
-    // the client we have here is unauthenticated.
-    // to do anything useful with the e-mails, we need to log in
-    let mut imap_session = client
-        .login(username, password)
-        .map_err(|e| e.0)?;
-
-    // we want to fetch the first email in the INBOX mailbox
-    // imap_session.select("INBOX")?;
-    imap_session.examine("INBOX")?;
-
-    Ok(imap_session)
+pub struct ImapSession<T: Read + Write> {
+    internal: Session<T>
 }
 
-pub fn close_session(mut imap_session: ImapSession) {
-    imap_session.logout().unwrap();
+impl<T: Read + Write> ImapSession<T> {
+    pub fn fetch<S1, S2>(&mut self, sequence_set : S1, query: S2) -> Result<ZeroCopy<Vec<Fetch>>, Error>
+        where S1: AsRef<str>, S2: AsRef<str>
+    {
+        self.internal.fetch(sequence_set, query)
+    }
+
+    pub fn close(mut self) {
+        self.internal.logout().unwrap();
+    }
+}
+
+impl ImapSession<TcpStream> {
+    pub fn open_session(domain: &str, port: u16, username: &str, password: &str) 
+        -> imap::error::Result<ImapSession<TcpStream>> {
+        let stream = TcpStream::connect((domain, port)).unwrap();
+        let client = imap::Client::new(stream);
+
+        // the client we have here is unauthenticated.
+        // to do anything useful with the e-mails, we need to log in
+        let mut internal = client
+            .login(username, password)
+            .map_err(|e| e.0)?;
+
+        internal.examine("INBOX")?;
+
+        Ok(ImapSession { internal })
+    }
 }
--- spikes/abstract-over-stream/src/main.rs
+++ spikes/abstract-over-stream/src/main.rs
@@ -9,8 +9,8 @@ fn main() {
     let args: Vec<String> = env::args().collect();
     let sequence_set = &args[1];
 
-    let mut imap_session: imap_session::ImapSession = 
-        imap_session::open_session(&config.imap.domain, config.imap.port, &config.imap.username, &password).unwrap();
+    let mut imap_session = 
+        imap_session::ImapSession::open_session(&config.imap.domain, config.imap.port, &config.imap.username, &password).unwrap();
 
     let messages = imap_session.fetch(sequence_set, "RFC822").unwrap();
     if let Some(message) = messages.iter().next() {
@@ -22,5 +22,5 @@ fn main() {
         println!("{}", body);
     }
 
-    imap_session::close_session(imap_session);
+    imap_session.close();
 }
